b1a5d9a45e97545081231c80a50bdaddbd59ecd2
spotlesscoder <codingspiderfox@gmail.com>
2026-03-08 19:41:31 +0100

    fix path

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -20,9 +20,9 @@ return {
     ft = "rust",
     config = function ()
       local mason_registry = require('mason-registry')
-      local codelldb = mason_registry.get_package("codelldb")
-      local extension_path = codelldb:get_install_path() .. "/extension/"
-      local codelldb_path = extension_path .. "adapter/codelldb"
+	  local codelldb_pkg = vim.fn.expand("$MASON/packages/codelldb")
+      local extension_path = codelldb_pkg .. "/extension/"
+	  local codelldb_path = extension_path .. "adapter/codelldb" 
       local liblldb_path = extension_path.. "lldb/lib/liblldb.dylib"
 	-- If you are on Linux, replace the line above with the line below:
 	-- local liblldb_path = extension_path .. "lldb/lib/liblldb.so"
